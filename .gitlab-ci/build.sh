97c39ad926808c5a979f3045ed7fdd51d62ec7b1
Clément DAVID <clement.david@3ds.com>
2022-09-07 09:27:46 +0200

    cleanup build

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-set -e
-
 svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/trunk/Dev-Tools/SE/Prerequirements/linux_x64/ scilab
 
 # configure & make
 cd scilab
 ./configure --prefix=''
-make -j
-make install DESTDIR=scilab-branch-"${CI_COMMIT_REF_NAME}"-"${CI_COMMIT_TIMESTAMP}"
+# use silent to avoid hitting the log limit
+make --silent --jobs >/dev/null
+make --silent install DESTDIR=scilab-branch-"${CI_COMMIT_REF_NAME}"-"${CI_COMMIT_TIMESTAMP}"  >/dev/null
