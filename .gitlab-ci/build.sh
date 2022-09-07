0a80c44af21ae922d6f40834a43f8e59f88b63c0
Clément DAVID <clement.david@3ds.com>
2022-09-07 11:31:11 +0200

    fix log.txt and nproc

--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+# log all commands to to avoid hitting Gitlab log limit
+
 svn checkout \
     --username anonymous --password Scilab \
     svn://svn.scilab.org/scilab/trunk/Dev-Tools/SE/Prerequirements/linux_x64/ scilab \
     |tee -a log.txt |tail --lines=1
 
 # configure & make
-cd scilab
-./configure --prefix='' |tee -a log.txt
-# use silent to avoid hitting the log limit
-make --jobs |tee -a log.txt |tail --lines=50
-make install DESTDIR=scilab-branch-"${CI_COMMIT_REF_NAME}"-"${CI_COMMIT_TIMESTAMP}" |tee -a log.txt |tail --lines=10
+cd scilab ||exit
+./configure --prefix='' |tee -a ../log.txt
+# nproc jobs limits memory usage
+make --jobs="$(nproc)" |tee -a ../log.txt |tail --lines=50
+make install DESTDIR=scilab-branch-"${CI_COMMIT_REF_NAME}"-"${CI_COMMIT_TIMESTAMP}" |tee -a ../log.txt |tail --lines=10
