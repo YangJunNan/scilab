f3485721edb5e4c93e7bc47bcf1661e18254372f
Clement DAVID <clement.david@3ds.com>
2022-09-15 17:06:12 +0200

    build with Gitlab CI env variables

--- .gitlab-ci/bld.bat
+++ .gitlab-ci/bld.bat
@@ -4,24 +4,39 @@ echo on
 svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/trunk/Dev-Tools/SE/Prerequirements/Windows_x64/ scilab >log.txt
 if errorlevel 1 exit 1
 
-cd scilab
-
 REM Define environment variables
 call "%VS2017INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" x64
 echo on
 call "%ONEAPI_ROOT%\setvars.bat" intel64 vs2017
 echo on
 setx SCILAB_JDK64 %JAVA_HOME%
-env
+
+cd scilab
+
+REM patch version numbers
+"c:\Program Files\ds_shell\code\bin\ds_shell\date.exe" +"%%s" >timestamp
+set /p NOW=<timestamp
+sed -i ^
+ -e 's/SCI_VERSION_STRING .*/SCI_VERSION_STRING ^"scilab-branch-%CI_COMMIT_BRANCH%^"/' ^
+ -e 's/SCI_VERSION_WIDE_STRING .*/SCI_VERSION_WIDE_STRING L^"scilab-branch-%CI_COMMIT_BRANCH%^"/' ^
+ -e 's/SCI_VERSION_REVISION .*/SCI_VERSION_REVISION ^"%CI_COMMIT_SHA%^"/' ^
+ -e 's/SCI_VERSION_TIMESTAMP .*/SCI_VERSION_TIMESTAMP %NOW%/' ^
+ modules\core\includes\version.h.vc
 
 REM build with Visual Studio and Intel compilers
 echo on
 devenv Scilab.sln /build "Release|x64" /out ..\log.txt
 if errorlevel 1 exit 1
+devenv.com Scilab.sln /build "Release|x64" /project buildhelp
+devenv.com Scilab.sln /build "Release|x64" /project buildjavadoc
 
-REM Package with innosetups
+REM Package with Inno Setup 6
 echo on
-call bin\scilab.bat -f tools\innosetup\Create_ISS.sce
+call bin\scilab.bat -nwni -f tools\innosetup\Create_ISS.sce
 if errorlevel 1 exit 1
 "C:\Program Files (x86)\Inno Setup 6\iscc.exe" tools\innosetup\Scilab.iss
+REM TODO: how to sign ? was:
+REM call d:\signtool_password.bat
+REM "C:\Program Files (x86)\Windows Kits\8.1\bin\x64\signtool.exe" sign /f D:\\ESIGroupCERT.pfx /p "%SIGNPASS%" /t http://timestamp.sectigo.com /v .\Output\scilab-branch-6.1_x64.exe
+move .\Output\scilab-branch-6.1_x64.exe scilab-branch-%CI_COMMIT_BRANCH%-%CI_COMMIT_TIMESTAMP%.exe
 if errorlevel 1 exit 1
--- .gitlab-ci/build.sh
+++ .gitlab-ci/build.sh
@@ -1,15 +1,68 @@
-#!/bin/sh
+#!/bin/env bash
 
-# log all commands to to avoid hitting Gitlab log limit
+# NOTE: log all commands to to avoid hitting Gitlab log limit
+# NOTE: nproc is used to limit memory usage
 
+# Remove install dir older than 3 days and TMPDIRs older than 1 day
+find "/tmp/scilab-branch*" -maxdepth 1 -mtime +2 -print0 |\
+while IFS= read -r -d '' i ; do
+  chmod -R +w "$i" && rm -rf "$i";
+done
+find /tmp -maxdepth 1 -mtime +0 -name "SCI_TMP*" -type d |\
+while IFS= read -r -d '' i ; do
+  [ -w "$i" ] && rm -rf "$i";
+done
+
+# checkout pre-requirements
 svn checkout \
     --username anonymous --password Scilab \
-    svn://svn.scilab.org/scilab/trunk/Dev-Tools/SE/Prerequirements/linux_x64/ scilab \
+    "svn://svn.scilab.org/scilab/${PREREQUIREMENTS_BRANCH}/Dev-Tools/SE/Prerequirements/linux_x64/" scilab \
     |tee -a log.txt |tail --lines=1
 
-# configure & make
+# patch version numbers
+date +"%s" >timestamp
+NOW=$(cat timestamp)
+sed -i \
+ -e "s/SCI_VERSION_STRING .*/SCI_VERSION_STRING \"scilab-branch-${CI_COMMIT_BRANCH}\"/" \
+ -e "s/SCI_VERSION_WIDE_STRING .*/SCI_VERSION_WIDE_STRING L\"scilab-branch-${CI_COMMIT_BRANCH}\"/" \
+ -e "s/SCI_VERSION_REVISION .*/SCI_VERSION_REVISION \"${CI_COMMIT_SHA}\"/" \
+ -e "s/SCI_VERSION_TIMESTAMP .*/SCI_VERSION_TIMESTAMP ${NOW}/" \
+ modules/core/includes/version.h.in
+
+# predefined env
+LD_LIBRARY_PATH=$(pwd)/usr/lib/ && export LD_LIBRARY_PATH
+DISPLAY=:0.0 && export DISPLAY
+
+# configure
 cd scilab ||exit
 ./configure --prefix='' |tee -a ../log.txt
-# nproc jobs limits memory usage
+
+# make 
 make --jobs="$(nproc)" |tee -a ../log.txt |tail --lines=50
-make install DESTDIR=scilab-branch-"${CI_COMMIT_REF_NAME}"-"${CI_COMMIT_TIMESTAMP}" |tee -a ../log.txt |tail --lines=10
+
+# install to tmpdir
+make install DESTDIR="/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}" |tee -a ../log.txt |tail --lines=10
+
+# copy thirdparties
+cp -a lib/thirdparty "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/lib/"
+cp -a thirdparty "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/"
+cp -a java/jre "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/thirdparty/java/"
+cp -a lib/thirdparty/libgluegen2-rt.so "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/bin/"
+cp -a lib/thirdparty/libnativewindow_awt.so "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/bin/"
+cp -a lib/thirdparty/libnativewindow_x11.so "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/bin/"
+cp -a lib/thirdparty/libnativewindow_x11.so "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/bin/"
+
+# Update the classpath
+sed -i "s#$(pwd)#\$SCILAB/../../#g" "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/share/scilab/etc/classpath.xml"
+
+# Update the rpath and ELF NEEDED
+cd "/tmp/scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}/" ||exit
+patchelf --set-rpath '$ORIGIN:$ORIGIN/../lib/scilab:$ORIGIN/../lib/thirdparty:$ORIGIN/../lib/thirdparty/redist' \
+					bin/scilab-cli-bin bin/scilab-bin
+find lib/scilab/*.so* -type f -exec patchelf --set-rpath '$ORIGIN:$ORIGIN/../thirdparty:$ORIGIN/../thirdparty/redist' {} \;
+readelf -d bin/scilab-cli-bin |awk '/NEEDED/{gsub(/\[/,""); gsub(/\]/,""); print "patchelf --add-needed "$NF" lib/scilab/libscilab-cli.so"}' |sh -
+readelf -d bin/scilab-bin |awk '/NEEDED/{gsub(/\[/,""); gsub(/\]/,""); print "patchelf --add-needed "$NF" lib/scilab/libscilab.so"}' |sh -
+cd "${CI_PROJECT_DIR}" ||exit
+
+# package as a tar gz file
+tar -xzf "scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}.tar.gz" -C /tmp "scilab-branch-${CI_COMMIT_BRANCH}-${CI_COMMIT_TIMESTAMP}"
