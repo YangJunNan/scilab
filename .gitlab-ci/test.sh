a3ba3200d314ebaf225f452d91c08c0cce65d267
Clement DAVID <clement.david@3ds.com>
2022-09-23 14:10:12 +0200

    tests: git checkout as test run use a script

--- .gitlab-ci/test.sh
+++ .gitlab-ci/test.sh
@@ -2,11 +2,11 @@
 # Execute module tests for a module name ${TEST}, download and install latest build if needed
 
 # Download if needed, install if needed
+if [ ! -f "${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz" ]; then
+    curl --output "${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz" --header "JOB-TOKEN: $CI_JOB_TOKEN" \
+        "https://gitlab.com/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/builds/artifacts/${CI_COMMIT_REF_NAME}/raw/${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz?job=build_${ARCH}_branch";
+fi
 if [ ! -d "${SCI_VERSION_STRING}" ]; then
-    if [ ! -f "${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz" ]; then
-        curl --output "${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz" --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-            "https://gitlab.com/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/builds/artifacts/${CI_COMMIT_REF_NAME}/raw/${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz?job=build_${ARCH}_branch";
-    fi
     tar -xzf "${SCI_VERSION_STRING}.bin.${ARCH}.tar.gz"
 fi
 
