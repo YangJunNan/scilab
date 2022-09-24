f0d01895c041a195b45c3faf5acf790cecc05069
Clement DAVID <clement.david@3ds.com>
2022-09-24 18:50:34 +0200

    debug build.env files

--- .gitlab-ci/test.bat
+++ .gitlab-ci/test.bat
@@ -1,10 +1,5 @@
 REM Execute module test for a module named %TEST%, download and install latest build if needed
 
-REM reset env from build.env
-curl --output "build.env" --header "JOB-TOKEN: $CI_JOB_TOKEN" \
-            "https://gitlab.com/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/builds/artifacts/${CI_COMMIT_REF_NAME}/raw/build.env?job=set_%ARCH%_windows_env";
-FOR /F "tokens=*" %%i in ('type build.env') do SET %%i
-
 REM Download if needed, install if needed
 if exist "%SCI_VERSION_STRING%"\ goto :test
 if exist "%SCI_VERSION_STRING%_%ARCH%.exe" goto :install
