e00eefceaef84f619c885a8e60b7838b4759114a
Clement DAVID <clement.david@3ds.com>
2022-09-25 17:54:21 +0200

    echo on test.bat

--- .gitlab-ci/test.bat
+++ .gitlab-ci/test.bat
@@ -1,5 +1,6 @@
 REM Execute module test for a module named %TEST%, download and install latest build if needed
 
+echo on
 REM Download if needed, install if needed
 if exist "%SCI_VERSION_STRING%"\ goto :test
 if exist "%SCI_VERSION_STRING%_%ARCH%.exe" goto :install
@@ -9,7 +10,9 @@ curl --output "%SCI_VERSION_STRING%_%ARCH%.exe" --header "JOB-TOKEN: $CI_JOB_TOK
             "https://gitlab.com/${CI_PROJECT_NAMESPACE}/${CI_PROJECT_NAME}/builds/artifacts/${CI_COMMIT_REF_NAME}/raw/%SCI_VERSION_STRING%_%ARCH%.exe?job=build_%ARCH%_windows_branch";
 
 :install
+echo on
 .\%SCI_VERSION_STRING%_%ARCH%.exe /SUPPRESSMSGBOXES /SILENT /SP- /DIR=%SCI_VERSION_STRING%
 
 :test
-.\%SCI_VERSION_STRING%\bin\scilab -nwni -quit -e 'test_run("'%TEST%'",[],[],"'%TEST%.xml'")'
+echo on
+call .\%SCI_VERSION_STRING%\bin\scilab.bat -nwni -quit -e 'test_run("'%TEST%'",[],[],"'%TEST%.xml'")'
