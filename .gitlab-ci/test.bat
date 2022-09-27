354febaae9c4766739da9ed738b6d910063a0272
Clement DAVID <clement.david@3ds.com>
2022-09-27 17:17:40 +0200

    debug windows install

--- .gitlab-ci/test.bat
+++ .gitlab-ci/test.bat
@@ -2,7 +2,8 @@ REM Execute module test for a module named %TEST%, download and install latest b
 
 echo on
 REM Always install
-.\%SCI_VERSION_STRING%_%ARCH%.exe /SUPPRESSMSGBOXES /SILENT /SP- /DIR=%SCI_VERSION_STRING%
+"%CI_PROJECT_DIR%\%SCI_VERSION_STRING%_%ARCH%.exe" /SUPPRESSMSGBOXES /SILENT /SP- /DIR="%CI_PROJECT_DIR%\%SCI_VERSION_STRING%"
+dir "%CI_PROJECT_DIR%\%SCI_VERSION_STRING%"
 
 @echo on
 setlocal EnableExtensions
@@ -13,6 +14,6 @@ set "SCIHOME=%tmp%\SCI_TMP_HOME~%RANDOM%"
 if exist -d "%SCIHOME%\" goto :uniqLoop
 mkdir "%SCIHOME%"
 
-call .\%SCI_VERSION_STRING%\bin\scilab.bat -nwni -scihome "%SCIHOME%" -quit -e 'test_run("'%TEST%'",[],[],"'%TEST%.xml'")'
+call %CI_PROJECT_DIR%\%SCI_VERSION_STRING%\bin\scilab.bat -nwni -scihome "%SCIHOME%" -quit -e 'test_run("'%TEST%'",[],[],"'%TEST%.xml'")'
 
 rmdir /s /q "%SCIHOME%"
