9e75e76433f720281689cad52b1bef45518bf950
Clement DAVID <clement.david@3ds.com>
2022-09-28 16:43:44 +0200

    debug robocopy #4

--- .gitlab-ci/build.bat
+++ .gitlab-ci/build.bat
@@ -30,7 +30,7 @@ sed -i ^
 
 REM FIXME copy intel redistribuables as thirdparties are not up to date
 robocopy /e /copy:DAT "%IFORT_COMPILER22%\redist\intel64_win\compiler\1033" "%CI_PROJECT_DIR%\scilab\bin\1033"
-robocopy /e /copy:DAT "%IFORT_COMPILER22%\redist\intel64_win\compiler" "%CI_PROJECT_DIR%\scilab\bin"
+robocopy /e /copy:DAT "%IFORT_COMPILER22%\redist\intel64_win\compiler" "%CI_PROJECT_DIR%\scilab\bin" ^
 libifcoremd.dll ^
 libifcoremdd.dll ^
 libifcorert.dll ^
