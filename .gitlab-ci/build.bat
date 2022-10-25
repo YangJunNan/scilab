07ead75226f5b3177c5c07021b5384519fe91db1
Cédric Delamarre <cedric.delamarre@3ds.com>
2022-10-25 12:22:13 +0200

    debug

--- .gitlab-ci/build.bat
+++ .gitlab-ci/build.bat
@@ -6,7 +6,7 @@ REM
 REM NOTE: log all commands to log.txt to avoid hitting Gitlab log limit
 
 svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/%PREREQUIREMENTS_BRANCH%/Dev-Tools/SE/Prerequirements/Windows_x64/ scilab >log_svn.txt
-if errorlevel 1 (tail --lines=100 ..\log_svn.txt 1>&2 & exit 1)
+if errorlevel 1 tail --lines=20 ..\log_svn.txt 1>&2 & exit 1
 REM display svn revision
 tail -n 1 log_svn.txt
 REM revert local modification
@@ -43,20 +43,20 @@ sed -i ^
 
 REM build with Visual Studio and Intel compilers
 devenv Scilab.sln /build "Release|x64" > ..\log_build.txt
-if errorlevel 1 tail --lines=100 ..\log_build.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 ..\log_build.txt 1>&2 & exit 1
 devenv Scilab.sln /build "Release|x64" /project buildhelp >..\log_buildhelp.txt |cmd /c ""
-if errorlevel 1 tail --lines=100 ..\log_buildhelp.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 ..\log_buildhelp.txt 1>&2 & exit 1
 devenv Scilab.sln /build "Release|x64" /project buildjavadoc >..\log_buildjavadoc.txt |cmd /c ""
-if errorlevel 1 tail --lines=100 ..\log_buildjavadoc.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 ..\log_buildjavadoc.txt 1>&2 & exit 1
 
 REM Delete previous builds
 del "%CI_PROJECT_DIR%\scilab*.exe" >nul 2>&1
 
 REM Package with Inno Setup 6
 bin\WScilex.exe -nb -f "tools\innosetup\Create_ISS.sce" >..\log_iss.txt
-if errorlevel 1 tail --lines=100 ..\log_iss.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 ..\log_iss.txt 1>&2 & exit 1
 "C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >>..\log_iss.txt
-if errorlevel 1 tail --lines=100 ..\log_iss.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 ..\log_iss.txt 1>&2 & exit 1
 
 move ".\Output\%SCI_VERSION_STRING%_%ARCH%.exe" "%CI_PROJECT_DIR%\%SCI_VERSION_STRING%_%ARCH%.exe"
 if "%ERRORLEVEL%" NEQ 0 exit 1
