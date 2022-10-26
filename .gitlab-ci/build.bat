683e823522c23e8ff39596cc543b298e8ab9a922
Cédric Delamarre <cedric.delamarre@3ds.com>
2022-10-26 09:59:59 +0200

    debug

--- .gitlab-ci/build.bat
+++ .gitlab-ci/build.bat
@@ -4,14 +4,18 @@ REM Copyright (C) Dassault Systemes - 2022 - Cedric DELAMARRE
 REM
 REM Builder script for building Scilab on Windows
 REM
-REM NOTE: log all commands to log.txt to avoid hitting Gitlab log limit
+REM NOTE: log all commands to log files to avoid hitting Gitlab log limit
+
+REM Create log folder
+set LOG_PATH="logs_%CI_COMMIT_SHORT_SHA%"
+mkdir "%LOG_PATH%"
 
 svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/%PREREQUIREMENTS_BRANCH%/Dev-Tools/SE/Prerequirements/Windows_x64/ scilab >log_svn.txt
-if errorlevel 1 tail --lines=20 ..\log_svn.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 "%LOG_PATH%\log_svn.txt" 1>&2 & exit 1
 REM display svn revision
-tail -n 1 log_svn.txt
+tail -n 1 "%LOG_PATH%\log_svn.txt"
 REM revert local modification
-svn revert -R scilab >>log_svn.txt
+svn revert -R scilab >> "%LOG_PATH%\log_svn.txt"
 
 call "%VS2017INSTALLDIR%\Common7\Tools\VsDevCmd.bat"
 @REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
@@ -40,20 +44,20 @@ sed -i ^
 @REM svml_dispmd.dll
 
 REM build with Visual Studio and Intel compilers
-devenv Scilab.sln /build "Release|x64" > ..\log_build.txt
-if errorlevel 1 tail --lines=20 ..\log_build.txt 1>&2 & exit 1
-devenv Scilab.sln /build "Release|x64" /project buildhelp >..\log_buildhelp.txt |cmd /c ""
-if errorlevel 1 tail --lines=20 ..\log_buildhelp.txt 1>&2 & exit 1
-devenv Scilab.sln /build "Release|x64" /project buildjavadoc >..\log_buildjavadoc.txt |cmd /c ""
-if errorlevel 1 tail --lines=20 ..\log_buildjavadoc.txt 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" > "%LOG_PATH%\..\log_build.txt
+if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_build.txt" 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" /project buildhelp >..\log_buildhelp.txt
+if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_buildhelp.txt" 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" /project buildjavadoc >..\log_buildjavadoc.txt
+if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_buildjavadoc.txt" 1>&2 & exit 1
 
 REM Delete previous builds
 del "%CI_PROJECT_DIR%\scilab*.exe" >nul 2>&1
 
 REM Package with Inno Setup 6
-bin\WScilex.exe -nb -f "tools\innosetup\Create_ISS.sce" >..\log_iss.txt
+bin\WScilex.exe -nb -f "tools\innosetup\Create_ISS.sce" > "%LOG_PATH%\..\log_iss.txt"
 if errorlevel 1 tail --lines=20 ..\log_iss.txt 1>&2 & exit 1
-"C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >>..\log_iss.txt
+"C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >> "%LOG_PATH%\..\log_iss.txt"
 if errorlevel 1 tail --lines=20 ..\log_iss.txt 1>&2 & exit 1
 
 move ".\Output\%SCI_VERSION_STRING%_%ARCH%.exe" "%CI_PROJECT_DIR%\%SCI_VERSION_STRING%_%ARCH%.exe"
