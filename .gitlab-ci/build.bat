ba1f7b162920aeed70986c8907c616423aa33db8
Cédric Delamarre <cedric.delamarre@3ds.com>
2022-10-26 12:08:00 +0200

    cleanup in .post stage

--- .gitlab-ci/build.bat
+++ .gitlab-ci/build.bat
@@ -7,15 +7,15 @@ REM
 REM NOTE: log all commands to log files to avoid hitting Gitlab log limit
 
 REM Create log folder
-set LOG_PATH="logs_%CI_COMMIT_SHORT_SHA%"
+set LOG_PATH=logs_%CI_COMMIT_SHORT_SHA%
 mkdir "%LOG_PATH%"
 
 svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/%PREREQUIREMENTS_BRANCH%/Dev-Tools/SE/Prerequirements/Windows_x64/ scilab > "%LOG_PATH%\log_svn.txt"
 if errorlevel 1 tail --lines=20 "%LOG_PATH%\log_svn.txt" 1>&2 & exit 1
 REM display svn revision
-tail -n 1 "%LOG_PATH%\log_svn.txt"
+tail -n 1 %LOG_PATH%\log_svn.txt
 REM revert local modification
-svn revert -R scilab >> "%LOG_PATH%\log_svn.txt"
+svn revert -R scilab >> %LOG_PATH%\log_svn.txt
 
 call "%VS2017INSTALLDIR%\Common7\Tools\VsDevCmd.bat"
 @REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat"
@@ -44,19 +44,19 @@ sed -i ^
 @REM svml_dispmd.dll
 
 REM build with Visual Studio and Intel compilers
-devenv Scilab.sln /build "Release|x64" > "%LOG_PATH%\..\log_build.txt
-if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_build.txt" 1>&2 & exit 1
-devenv Scilab.sln /build "Release|x64" /project buildhelp >..\log_buildhelp.txt
-if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_buildhelp.txt" 1>&2 & exit 1
-devenv Scilab.sln /build "Release|x64" /project buildjavadoc >..\log_buildjavadoc.txt
-if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_buildjavadoc.txt" 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" > %LOG_PATH%\..\log_build.txt
+if errorlevel 1 tail --lines=20 %LOG_PATH%\..\log_build.txt 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" /project buildhelp > %LOG_PATH%\..\log_buildhelp.txt
+if errorlevel 1 tail --lines=20 %LOG_PATH%\..\log_buildhelp.txt 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" /project buildjavadoc > %LOG_PATH%\..\log_buildjavadoc.txt
+if errorlevel 1 tail --lines=20 %LOG_PATH%\..\log_buildjavadoc.txt 1>&2 & exit 1
 
 REM Package with Inno Setup 6
-bin\WScilex.exe -nb -f "tools\innosetup\Create_ISS.sce" > "%LOG_PATH%\..\log_iss.txt"
-if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_iss.txt" 1>&2 & exit 1
-"C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >> "%LOG_PATH%\..\log_iss.txt"
-if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_iss.txt" 1>&2 & exit 1
-if errorlevel 1 tail --lines=20 "%LOG_PATH%\..\log_iss.txt" 1>&2 & exit 1
+bin\WScilex.exe -nb -f "tools\innosetup\Create_ISS.sce" > %LOG_PATH%\..\log_iss.txt
+if errorlevel 1 tail --lines=20 %LOG_PATH%\..\log_iss.txt 1>&2 & exit 1
+"C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >> %LOG_PATH%\..\log_iss.txt
+if errorlevel 1 tail --lines=20 %LOG_PATH%\..\log_iss.txt 1>&2 & exit 1
+if errorlevel 1 tail --lines=20 %LOG_PATH%\..\log_iss.txt 1>&2 & exit 1
 
 move ".\Output\%SCI_VERSION_STRING%_%ARCH%.exe" "%CI_PROJECT_DIR%\%SCI_VERSION_STRING%_%ARCH%.exe"
 if errorlevel 1 exit 1
