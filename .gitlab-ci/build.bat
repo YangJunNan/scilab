e1bc8b2f4e57b75c6b6046d06b712c928d81b956
Clément DAVID <clement.david@3ds.com>
2022-12-16 14:47:51 +0000

    CI: tag log files with commit hash to ease further analysis
    After downloading a lot of log files, this is hard to compare without having a commit hash in its file. This commit adds it to all log files.
    
    See https://gitlab.com/scilab/scilab/-/merge_requests/73

--- .gitlab-ci/build.bat
+++ .gitlab-ci/build.bat
@@ -16,13 +16,13 @@ set LOG_PATH=logs_%CI_COMMIT_SHORT_SHA%
 if not exist %LOG_PATH% mkdir %LOG_PATH%
 
 curl -k -z prereq.zip -o prereq.zip https://oos.eu-west-2.outscale.com/scilab-releases-dev/prerequirements/prerequirements-scilab-branch-6.1-windows_x64.zip
-unzip -o prereq.zip -d scilab > %LOG_PATH%\log_prereq.txt
-@REM svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/%PREREQUIREMENTS_BRANCH%/Dev-Tools/SE/Prerequirements/Windows_x64/ scilab > %LOG_PATH%\log_svn.txt
-@REM if errorlevel 1 tail --lines=20 %LOG_PATH%\log_svn.txt 1>&2 & exit 1
+unzip -o prereq.zip -d scilab > %LOG_PATH%\log_prereq_%CI_COMMIT_SHORT_SHA%.txt
+@REM svn checkout --username anonymous --password Scilab svn://svn.scilab.org/scilab/%PREREQUIREMENTS_BRANCH%/Dev-Tools/SE/Prerequirements/Windows_x64/ scilab > %LOG_PATH%\log_svn_%CI_COMMIT_SHORT_SHA%.txt
+@REM if errorlevel 1 tail --lines=20 %LOG_PATH%\log_svn_%CI_COMMIT_SHORT_SHA%.txt 1>&2 & exit 1
 @REM REM display svn revision
-@REM tail -n 1 %LOG_PATH%\log_svn.txt
+@REM tail -n 1 %LOG_PATH%\log_svn_%CI_COMMIT_SHORT_SHA%.txt
 @REM REM revert local modification
-@REM svn revert -R scilab >> %LOG_PATH%\log_svn.txt
+@REM svn revert -R scilab >> %LOG_PATH%\log_svn_%CI_COMMIT_SHORT_SHA%.txt
 
 REM Define environment variables
 set SCILAB_JDK64=%JAVA_HOME%
@@ -44,18 +44,18 @@ if exist modules\core\includes\version.h sed -i ^
  modules\core\includes\version.h
 
 REM build with Visual Studio and Intel compilers
-devenv Scilab.sln /build "Release|x64" > ..\%LOG_PATH%\log_build.txt
-if errorlevel 1 tail --lines=20 ..\%LOG_PATH%\log_build.txt 1>&2 & exit 1
-devenv Scilab.sln /build "Release|x64" /project buildhelp > ..\%LOG_PATH%\log_buildhelp.txt
-devenv Scilab.sln /build "Release|x64" /project buildjavadoc > ..\%LOG_PATH%\log_buildjavadoc.txt
+devenv Scilab.sln /build "Release|x64" > ..\%LOG_PATH%\log_build_%CI_COMMIT_SHORT_SHA%.txt
+if errorlevel 1 tail --lines=20 ..\%LOG_PATH%\log_build_%CI_COMMIT_SHORT_SHA%.txt 1>&2 & exit 1
+devenv Scilab.sln /build "Release|x64" /project buildhelp > ..\%LOG_PATH%\log_buildhelp_%CI_COMMIT_SHORT_SHA%.txt
+devenv Scilab.sln /build "Release|x64" /project buildjavadoc > ..\%LOG_PATH%\log_buildjavadoc_%CI_COMMIT_SHORT_SHA%.txt
 
 REM Package with Inno Setup 6
 if exist "Scilab.iss" del /f "Scilab.iss"
-bin\WScilex-cli.exe -nb -f "tools\innosetup\Create_ISS.sce" > ..\%LOG_PATH%\log_iss.txt
-if errorlevel 1 tail --lines=20 ..\%LOG_PATH%\log_iss.txt 1>&2 & exit 1
+bin\WScilex-cli.exe -nb -f "tools\innosetup\Create_ISS.sce" > ..\%LOG_PATH%\log_iss_%CI_COMMIT_SHORT_SHA%.txt
+if errorlevel 1 tail --lines=20 ..\%LOG_PATH%\log_iss_%CI_COMMIT_SHORT_SHA%.txt 1>&2 & exit 1
 if not exist "Scilab.iss" exit 1
-"C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >> ..\%LOG_PATH%\log_iss.txt
-if errorlevel 1 tail --lines=20 ..\%LOG_PATH%\log_iss.txt 1>&2 & exit 1
+"C:\Program Files (x86)\Inno Setup 6\iscc.exe" Scilab.iss >> ..\%LOG_PATH%\log_iss_%CI_COMMIT_SHORT_SHA%.txt
+if errorlevel 1 tail --lines=20 ..\%LOG_PATH%\log_iss_%CI_COMMIT_SHORT_SHA%.txt 1>&2 & exit 1
 
 move ".\Output\%SCI_VERSION_STRING%_%ARCH%.exe" "%CI_PROJECT_DIR%\%SCI_VERSION_STRING%.bin.%ARCH%.exe"
 if errorlevel 1 exit 1
