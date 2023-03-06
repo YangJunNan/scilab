414385cf5de55e0aadc853c131348ccd0132da17
Cédric DELAMARRE <cedric.delamarre@3ds.com>
2023-03-06 11:31:34 +0000

    library tostring fixed
    On Linux, wrong functions files location
    ```
    --> corelib 
     corelib  = 
    
    Functions files location : S.
    
    ...
    ```
    
    See https://gitlab.com/scilab/scilab/-/merge_requests/228

--- scilab/modules/ast/src/cpp/types/library.cpp
+++ scilab/modules/ast/src/cpp/types/library.cpp
@@ -60,7 +60,7 @@ Library::~Library()
 bool Library::toString(std::wostringstream& ostr)
 {
     wchar_t output[1024] = {0};
-    os_swprintf(output, 1024, _W("Functions files location : %s.\n").c_str(), m_wstPath.c_str());
+    os_swprintf(output, 1024, _W("Functions files location : %ls.\n").c_str(), m_wstPath.c_str());
 
     ostr << output << std::endl;
 
