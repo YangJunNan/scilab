7e9f76779b85a733349381e223265af2a0649385
Cédric DELAMARRE <cedric.delamarre@3ds.com>
2023-10-16 17:07:01 +0200

    CI - set tests results in artifact

--- .gitlab-ci/test.sh
+++ .gitlab-ci/test.sh
@@ -24,7 +24,7 @@ LOG_PATH=$SCI_VERSION_STRING
 [ ! -d "$LOG_PATH" ] && mkdir "$LOG_PATH"
 
 echo -e "\e[0Ksection_start:$(date +%s):test\r\e[0KTesting $TEST"
-xvfb-run $HOME/${SCI_VERSION_STRING}/bin/scilab -nw -quit -e 'test_run("'"${TEST}"'",[],[],"'"${LOG_PATH}/${TEST}.xml"'")'
+xvfb-run $HOME/${SCI_VERSION_STRING}/bin/scilab -nw -quit -e 'test_run("'"${TEST}"'",[],[],"'"${LOG_PATH}/${TEST}.xml"'"), copyfile(TMPDIR+"/test_run_result/", "'"${LOG_PATH}/"'");'
 echo -e "\e[0Ksection_end:$(date +%s):test\r\e[0K"
 
 # fail without xml report
