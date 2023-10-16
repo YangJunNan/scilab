YangJunNan/scilab

commit f985eb42de65f5a25f0b681fcaa474f6509bfb21

Use native filechooser from JavaFX on macOS
Use JavaFX FileChooser for all Scilab popups on macOS. The switch between JavaFX and Swing FileChooser is done at the lowest possible level (in final bridge class). To achieve this the Scilab FileChooser interface is enriched by missing methods in order to replace explicit uses of SwingScilabFileChooser in gui, scinotes and xcos modules.

Fix: #17110, #8339

Co-authored-by: Clement DAVID <clement.david@3ds.com>
Co-authored-by: Antoine ELIAS <antoine.elias@3ds.com>
Co-authored-by: Vincent COUVERT <vincent.couvert@3ds.com>
Closes #8339 and #17110
See https://gitlab.com/scilab/scilab/-/merge_requests/493

scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/filechooser/SwingScilabFileChooser.java
@@ -26,6 +26,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileFilter;
 
 import org.scilab.modules.gui.filechooser.FileChooserInfos;
 import org.scilab.modules.gui.filechooser.SimpleFileChooser;
@@ -131,6 +132,16 @@ public class SwingScilabFileChooser extends JFileChooser implements SimpleFileCh
     }
 
     /**
+     * Set the initial file name
+     * @param path the initial file name
+     */
+    public void setInitialFileName(String path) {
+        if (path != null) {
+            super.setSelectedFile(new File(path));
+        }
+     }
+
+    /**
      * Set the parent frame
      * @param parent the parent frame
      */
@@ -180,7 +191,11 @@ public class SwingScilabFileChooser extends JFileChooser implements SimpleFileCh
                 }
             } else {
                 File file = this.getSelectedFile();
-
+                FileFilter fileFilter = this.getFileFilter();
+                if (!fileFilter.accept(file)) {
+                    String[] extensions = ((SciFileFilter)fileFilter).getExtensions();
+                    file = new File(file.getPath() + "." + extensions[0]);
+                }
                 if (this.dialogType == JFileChooser.SAVE_DIALOG) {
                     //Test if there is a file with the same name
                     if (file.exists()) {
@@ -311,6 +326,20 @@ public class SwingScilabFileChooser extends JFileChooser implements SimpleFileCh
     @Override
     public String[] getSelectionFileNames() {
         return selectionFileNames;
+    }
+
+    /**
+     * Get the user-selected extension from the dialog
+     * @return the user-selected extension from the dialog
+     */
+    public String getSelectedExtension()
+    {
+        String ext = "";
+        FileFilter filter = getFileFilter();
+        if (filter instanceof javax.swing.filechooser.FileNameExtensionFilter) {
+            ext = ((javax.swing.filechooser.FileNameExtensionFilter)filter).getExtensions()[0];
+        }
+        return ext;
     }
 
     /**

commit f2eea82ab776408c0f8ac1291b9a6a2a3cbbfdc7

fix an Xcos save issue introduced by f985eb42
See https://gitlab.com/scilab/scilab/-/merge_requests/645

scilab/modules/gui/src/java/org/scilab/modules/gui/bridge/filechooser/SwingScilabFileChooser.java
@@ -194,7 +194,9 @@ public class SwingScilabFileChooser extends JFileChooser implements SimpleFileCh
                 FileFilter fileFilter = this.getFileFilter();
                 if (!fileFilter.accept(file)) {
                     String[] extensions = ((SciFileFilter)fileFilter).getExtensions();
-                    file = new File(file.getPath() + "." + extensions[0]);
+                    if (extensions != null && extensions.length > 0) {
+                        file = new File(file.getPath() + "." + extensions[0]);
+                    }
                 }
                 if (this.dialogType == JFileChooser.SAVE_DIALOG) {
                     //Test if there is a file with the same name