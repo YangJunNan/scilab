902d4c9bb52d9be0e89d1345e9c2632ecb01c144
Stéphane MOTTELET <stephane.mottelet@utc.fr>
2023-10-17 16:14:55 +0000

    Eventual varargout symbol should be removed as others.

--- scilab/modules/core/sci_gateway/cpp/sci_checkNamedArguments.cpp
+++ scilab/modules/core/sci_gateway/cpp/sci_checkNamedArguments.cpp
@@ -81,6 +81,7 @@ types::Function::ReturnValue sci_checkNamedArguments(types::typed_list &in, int 
     lst.remove_if([](const lst_entry_t& p) -> bool { return p.first == L"nargin"; });
     lst.remove_if([](const lst_entry_t& p) -> bool { return p.first == L"nargout"; });
     lst.remove_if([](const lst_entry_t& p) -> bool { return p.first == L"varargin"; });
+    lst.remove_if([](const lst_entry_t& p) -> bool { return p.first == L"varargout"; });
 
     for (int i = 0; i < size; ++i)
     {
