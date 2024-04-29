af2718b369a31fa73dcca86c9146d033f2efb217
Vincent COUVERT <vincent.couvert@3ds.com>
2024-04-29 17:36:02 +0200

    Re-license file to BSD-3.

--- scilab/modules/javasci/src/java/org/scilab/modules/javasci/Scilab.java
+++ scilab/modules/javasci/src/java/org/scilab/modules/javasci/Scilab.java
@@ -5,7 +5,10 @@
  *
  * Copyright (C) 2012 - 2016 - Scilab Enterprises
  *
- * This file is hereby licensed under the terms of the GNU GPL v2.0,
+ * This file is hereby released under the 3-clause BSD license.
+ * See COPYING-BSD.
+ * 
+ * This file was previously licensed under the terms of the GNU GPL v2.0,
  * pursuant to article 5.3.4 of the CeCILL v.2.1.
  * This file was originally licensed under the terms of the CeCILL v2.1,
  * and continues to be available under such terms.
